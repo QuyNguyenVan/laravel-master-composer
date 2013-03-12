932b8257618070e36e10993212ed7a81afeab387
Jason <j20git@gmail.com>
2013-03-12 16:15:40 -0500

    Added nav links for: My Account & Logout / Sign Up & Login

--- master.blade.php
+++ master.blade.php
@@ -43,9 +43,13 @@
                     <a class="brand" href="/">Logo</a>
                     <div class="nav-collapse collapse">
                         <ul class="nav pull-right">
-                            <li class="active"><a href="/">Home</a></li>
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/contact">Contact</a></li>
+                            @if (Auth::check())
+                                <li><a href="/member" >My Account</a></li>
+                                <li><a href="/logout">Logout</a></li>
+                            @else
+                                <li><a href="/signup" >Sign Up</a></li>
+                                <li><a href="/login">Login</a></li>
+                            @endif
                         </ul>
                     </div>
                 </div>
